42cf9b78fcf788a76972e6db9af9c2902e1294be
T-Odaka <59603125+T-Odaka@users.noreply.github.com>
2020-10-10 13:00:05 +0900

    merge

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::resource('/potato', PotatoController::class);
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
