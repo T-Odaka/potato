74f25ca960437ec3894db5319a36d18b68817e67
T-Odaka <59603125+T-Odaka@users.noreply.github.com>
2020-10-14 19:43:16 +0900

    delete index.phpjs fontawsome, horizon card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,3 +13,102 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
 
+@section('content')
+
+<div class="container">
+    <div class="wrapper">
+
+        <!-- ソート用ボタン -->
+        <div class="dropdown offset-10 col-2">
+            <button type="button" id="dropdown1" class="dropdown-toggle btn-flat-dashed-border" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                並び替え
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdown1">
+                <a class="dropdown-item" href="/sortHot">名前</a>
+                <a class="dropdown-item" href="/sortHot">辛さ</a>
+                <a class="dropdown-item" href="/sortCrispy">サクサクさ</a>
+                <a class="dropdown-item" href="/sortSalt">塩分み</a>
+                <a class="dropdown-item" href="/sortGarlic">ニンニク度</a>
+            </div>
+        </div>
+
+        <!-- 垂直カード -->
+        <div class="row">
+            <div class="card-deck mx-auto">
+                @foreach ($pages as $page)
+                <div class="col-xl-4 col-md-6 col-sm-12  grid">
+
+                    <!-- カード本体 -->
+                    <div class="card border-0 shadow-sm">
+                        <?php
+                        $str = $page->id;
+                        $data = [$page->hot, $page->garlic, $page->salt, $page->crispy];
+                        $detail = [$page->description, $page->image_url];
+                        echo "<img class=card-img-top src=$detail[1] alt=$page->name >"
+                        ?>
+                        <div class="card-body">
+                            <div class="hukidashi-title">
+                                <img src="img/e0520_0.png">
+                                <h5>{{ $page->name }}</h5>
+                            </div>
+                            <p class="card-text"><?php echo $detail[0] ?></p>
+                            <div>
+                                <canvas id="<?php echo  $str ?>"></canvas>
+                            </div>
+
+                            <!-- レーダーチャート -->
+                            <script>
+                                var ctx = document.getElementById("<?php echo $str ?>");
+                                var myRadarChart = new Chart(ctx, {
+                                    type: 'radar',
+                                    data: {
+                                        labels: ["辛さ", "ニンニク度", "塩分み", "サクサクさ"],
+                                        datasets: [{
+                                            data: [<?php echo $data[0] . ',' . $data[1] . ',' . $data[2] . ',' . $data[3] ?>],
+                                            backgroundColor: 'RGBA(225,95,150, 0.5)',
+                                            borderColor: 'RGBA(225,95,150, 1)',
+                                            borderWidth: 1,
+                                            pointBackgroundColor: 'RGB(46,106,177)',
+                                            pointRadius: 2,
+                                        }]
+                                    },
+                                    options: {
+                                        animation: {
+                                            duration: 3000,
+                                            easing: "easeOutBounce",
+                                        },
+                                        legend: {
+                                            display: false,
+                                        },
+
+                                        title: {
+                                            display: false,
+                                        },
+                                        scale: {
+                                            label: {
+                                                display: false,
+                                            },
+                                            ticks: {
+                                                suggestedMin: 0,
+                                                suggestedMax: 5,
+                                                stepSize: 1,
+                                            },
+                                        }
+                                    }
+                                });
+                            </script>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+
+        <!-- ページネーション -->
+        <div class="d-flex justify-content-center col-3 mx-auto">
+            {{ $pages->appends(request()->query())->links('vendor.pagination.bootstrap-4') }}
+        </div>
+        <div id="page_top"><a href="#"></a></div>
+    </div>
+</div>
+@endsection
