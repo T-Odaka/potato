d7ea0405a713fc2eca2df1f7c10a813b04abeb3a
T-Odaka <59603125+T-Odaka@users.noreply.github.com>
2020-10-20 01:23:04 +0900

    add confirm message background

--- resources/views/complete.blade.php
+++ resources/views/complete.blade.php
@@ -7,8 +7,11 @@
 <div class="container">
     <div class="wrapper">
         <div class="card border-0 shadow-sm">
-            <p>お問い合わせありがとうございます。内容を確認次第返信いたします。</p>
-            <p>5秒後自動でトップページに遷移いたします。</p>
+            <div class="card-header">送信完了</div>
+            <div class="card-body">
+                <p>お問い合わせありがとうございます。内容を確認次第返信いたします。</p>
+                <p>5秒後自動でトップページに遷移いたします。</p>
+            </div>
         </div>
     </div>
 </div>
