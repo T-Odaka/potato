06173bac49781b25dc4afe593093d7a88a7fcac9
T-Odaka <59603125+T-Odaka@users.noreply.github.com>
2020-10-20 00:53:09 +0900

    auth email.php fix

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -38,7 +38,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('再設定用リンクの送信'') }}
+                                        {{ __('再設定用リンクの送信') }}
                                     </button>
                                 </div>
                             </div>
